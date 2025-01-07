860d3dc7e624d17b13eb76645cb86dd2443f5992
Uzakariyya <talk2geoumar@gmail.com>
2025-01-07 11:47:43 +0100

    Updated Assingment

---
+++ Assignment_6/c_d.py
@@ -0,0 +1,163 @@
+
+countries = [
+    {
+        "name": "Afghanistan",
+        "capital": "Kabul",
+        "languages": ["Pashto", "Uzbek", "Turkmen"],
+        "population": 27657145,
+        "flag": "https://restcountries.eu/data/afg.svg",
+        "currency": "Afghan afghani"
+    },
+    {
+        "name": "Uganda",
+        "capital": "Kampala",
+        "languages": ["English", "Swahili"],
+        "population": 33860700,
+        "flag": "https://restcountries.eu/data/uga.svg",
+        "currency": "Ugandan shilling"
+    },
+    {
+        "name": "Ukraine",
+        "capital": "Kiev",
+        "languages": ["Ukrainian"],
+        "population": 42692393,
+        "flag": "https://restcountries.eu/data/ukr.svg",
+        "currency": "Ukrainian hryvnia"
+    },
+    {
+        "name": "United Arab Emirates",
+        "capital": "Abu Dhabi",
+        "languages": ["Arabic"],
+        "population": 9856000,
+        "flag": "https://restcountries.eu/data/are.svg",
+        "currency": "United Arab Emirates dirham"
+    },
+    {
+        "name": "United Kingdom of Great Britain and Northern Ireland",
+        "capital": "London",
+        "languages": ["English"],
+        "population": 65110000,
+        "flag": "https://restcountries.eu/data/gbr.svg",
+        "currency": "British pound"
+    },
+    {
+        "name": "United States of America",
+        "capital": "Washington, D.C.",
+        "languages": ["English"],
+        "population": 323947000,
+        "flag": "https://restcountries.eu/data/usa.svg",
+        "currency": "United States dollar"
+    },
+    {
+        "name": "Uruguay",
+        "capital": "Montevideo",
+        "languages": ["Spanish"],
+        "population": 3480222,
+        "flag": "https://restcountries.eu/data/ury.svg",
+        "currency": "Uruguayan peso"
+    },
+    {
+        "name": "Uzbekistan",
+        "capital": "Tashkent",
+        "languages": ["Uzbek", "Russian"],
+        "population": 31576400,
+        "flag": "https://restcountries.eu/data/uzb.svg",
+        "currency": "Uzbekistani so'm"
+    },
+    {
+        "name": "Vanuatu",
+        "capital": "Port Vila",
+        "languages": ["Bislama", "English", "French"],
+        "population": 277500,
+        "flag": "https://restcountries.eu/data/vut.svg",
+        "currency": "Vanuatu vatu"
+    },
+    {
+        "name": "Venezuela (Bolivarian Republic of)",
+        "capital": "Caracas",
+        "languages": ["Spanish"],
+        "population": 31028700,
+        "flag": "https://restcountries.eu/data/ven.svg",
+        "currency": "Venezuelan bolívar"
+    },
+    {
+        "name": "Viet Nam",
+        "capital": "Hanoi",
+        "languages": ["Vietnamese"],
+        "population": 92700000,
+        "flag": "https://restcountries.eu/data/vnm.svg",
+        "currency": "Vietnamese đồng"
+    },
+    {
+        "name": "Wallis and Futuna",
+        "capital": "Mata-Utu",
+        "languages": ["French"],
+        "population": 11750,
+        "flag": "https://restcountries.eu/data/wlf.svg",
+        "currency": "CFP franc"
+    },
+    {
+        "name": "Western Sahara",
+        "capital": "El Aaiún",
+        "languages": ["Spanish"],
+        "population": 510713,
+        "flag": "https://restcountries.eu/data/esh.svg",
+        "currency": "Moroccan dirham"
+    },
+    {
+        "name": "Yemen",
+        "capital": "Sana'a",
+        "languages": ["Arabic"],
+        "population": 27478000,
+        "flag": "https://restcountries.eu/data/yem.svg",
+        "currency": "Yemeni rial"
+    },
+    {
+        "name": "Zambia",
+        "capital": "Lusaka",
+        "languages": ["English"],
+        "population": 15933883,
+        "flag": "https://restcountries.eu/data/zmb.svg",
+        "currency": "Zambian kwacha"
+    },
+    {
+        "name": "Zimbabwe",
+        "capital": "Harare",
+        "languages": ["English", "Shona", "Northern Ndebele"],
+        "population": 14240168,
+        "flag": "https://restcountries.eu/data/zwe.svg",
+        "currency": "Botswana pula"
+    },
+    {
+        "name": "Australia",
+        "capital": "Canberra",
+        "languages": ["English"],
+        "population": 25687041,
+        "flag": "https://restcountries.eu/data/aus.svg",
+        "currency": "Australian dollar"
+    },
+    {
+        "name": "Brazil",
+        "capital": "Brasília",
+        "languages": ["Portuguese"],
+        "population": 209288278,
+        "flag": "https://restcountries.eu/data/bra.svg",
+        "currency": "Brazilian real"
+    },
+    {
+        "name": "Canada",
+        "capital": "Ottawa",
+        "languages": ["English", "French"],
+        "population": 36430000,
+        "flag": "https://restcountries.eu/data/can.svg",
+        "currency": "Canadian dollar"
+    },
+    {
+        "name": "China",
+        "capital": "Beijing",
+        "languages": ["Chinese"],
+        "population": 1393780000,
+        "flag": "https://restcountries.eu/data/chn.svg",
+        "currency": "Renminbi"
+    }   
+]
